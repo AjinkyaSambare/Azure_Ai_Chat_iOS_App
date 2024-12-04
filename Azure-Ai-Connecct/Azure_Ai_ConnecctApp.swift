82a8f18e13f650de2969c15d9070b8e02c1b1ada
AjinkyaSambare <ajinkya@idealabs.fyi>
2024-12-05 00:30:36 +0530

    Initial Commit

---
+++ Azure-Ai-Connecct/Azure_Ai_ConnecctApp.swift
@@ -0,0 +1,17 @@
+//
+//  Azure_Ai_ConnecctApp.swift
+//  Azure-Ai-Connecct
+//
+//  Created by Ajinkya  on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Azure_Ai_ConnecctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
